894531d6fd997666f12a6d50e8871630fd1356ae
nathanshiaulam <nlam@princeton.edu>
2015-04-12 21:13:19 -0400

    Initial Commit

---
+++ Noms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noms
+//
+//  Created by Nathan Lam on 4/12/15.
+//  Copyright (c) 2015 COS333. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
