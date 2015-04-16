8d591353e9718d9db7abc4bf43d39383528222cc
nathanshiaulam <nlam@princeton.edu>
2015-04-16 04:11:10 -0400

    there's an unknown bug with the push segues

--- Noms/NameProfileViewController.swift
+++ Noms/NameProfileViewController.swift
@@ -1,33 +1,31 @@
 //
-//  PreferenceMenuViewController.swift
+//  NameProfileViewController.swift
 //  Noms
 //
-//  Created by Nathan Lam on 4/14/15.
+//  Created by Nathan Lam on 4/16/15.
 //  Copyright (c) 2015 COS333. All rights reserved.
 //
 
 import UIKit
-import Parse
 
-class PreferenceMenuViewController: UIViewController {
-    
-    @IBAction func onClickSaveButton(sender: UIButton) {
-        
-        
-    }
+class NameProfileViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
     override func viewDidLoad() {
-        
-   
+        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func textFieldShouldReturn(textField: UITextField)-> Bool {
+        textField.resignFirstResponder();
+        self.performSegueWithIdentifier("nameToPreferences", sender: self);
+        return true;
+    }
 
     /*
     // MARK: - Navigation
