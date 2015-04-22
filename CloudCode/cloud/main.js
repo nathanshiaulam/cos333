2bdbcdf9ce8512ee3eb07f2bd1650405eb9857de
anniechu <anyuanc@princeton.edu>
2015-04-21 22:44:27 -0400

    some main

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -6,9 +6,41 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 Parse.Cloud.define("findRestaurant", function(request, response) {
+<<<<<<< Updated upstream
   response.success("Hello world!");
 });
 
 Parse.Cloud.define("MatchRestaurant", function(request, response) {
 
 });
+=======
+  	var distance = request.params.distance;
+  	var currloc = request.params.loc;
+  	var cost = request.params.cost;
+  	var cuisine = request.params.cuisine;
+  	var skipnum = 0;
+	var query = new Parse.Query("Restaurant");
+	var results = [];
+	var foundall = false;
+	while (!foundall) {
+		query.limit(1000);
+		query.skip(skipnum);
+		query.find({
+		  	success: function(res) {
+		  		results = results.concat(res);
+		  		if (res.length !=== 1000) {
+		  			foundall = true;
+		  		}
+		  	},
+		  	error: function(error) {
+		    	alert("Error: " + error.code + " " + error.message);
+		  	}
+		});
+		skipnum = skipnum + 1000;
+	}
+	for (var i = 0; i < results.length; ++i) {
+		var rest = results[i];
+        var distbetween = 
+    }
+});
+>>>>>>> Stashed changes
