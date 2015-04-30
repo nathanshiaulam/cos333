f48e2c19d17e8505817209bcc1b5d2eb09684cd4
nathanshiaulam <nlam@princeton.edu>
2015-04-30 03:52:25 -0400

    made changes to view controllers/storyboard

--- Noms/OptionalMenuViewController.swift
+++ Noms/OptionalMenuViewController.swift
@@ -1,14 +1,14 @@
 //
-//  RestaurantDisplayViewController.swift
+//  OptionalMenuViewController.swift
 //  Noms
 //
-//  Created by Nathan Lam on 4/23/15.
+//  Created by Nathan Lam on 4/30/15.
 //  Copyright (c) 2015 COS333. All rights reserved.
 //
 
 import UIKit
 
-class RestaurantDisplayViewController: UIViewController {
+class OptionalMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
